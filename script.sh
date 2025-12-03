d5371b3dc6cbd7839113d97bdf86538d2bfebec6
JuanFer2007 <jupindore@uide.edu.ec>
2025-12-03 11:00:57 -0500

    Update script.h

--- script.sh
+++ script.sh
@@ -3,5 +3,5 @@
 echo "Hola Linux" > archivo.txt #echo: Muestra Texto en Pantalla
 cat archivo.txt #cat: Muestra contenido completo de un Archivo
 cp archivo.txt copia.txt #cp: Copia archivos o directorios
-ls -l # Lista archivos y directorios 
+ls -l # Lista archivos y directorios #-l:Formato largo muestra detalles
 rm archivo.txt #rm: elimina (remove) archivos
