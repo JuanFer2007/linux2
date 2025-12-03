bbac4b97639eed3a8e206f66635ab8140cde34eb
JuanFer2007 <jupindore@uide.edu.ec>
2025-12-03 15:41:48 +0000

    Script Explicado

---
+++ script.sh
@@ -0,0 +1,7 @@
+#Crear y Listar archivos
+
+echo "Hola Linux" > archivo.txt #echo: Muestra Texto en Pantalla
+cat archivo.txt #cat: Muestra contenido completo de un Archivo
+cp archivo.txt copia.txt #cp: Copia archivos o directorios
+ls -l # Lista archivos y directorios 
+rm archivo.txt
