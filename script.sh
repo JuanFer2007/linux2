335a44d13cb5901895c180d73622d9d9e435a9e3
JuanFer2007 <jupindore@uide.edu.ec>
2025-12-03 15:54:21 +0000

    Script2

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ echo "Hola Linux" > archivo.txt #echo: Muestra Texto en Pantalla
 cat archivo.txt #cat: Muestra contenido completo de un Archivo
 cp archivo.txt copia.txt #cp: Copia archivos o directorios
 ls -l # Lista archivos y directorios 
-rm archivo.txt
+rm archivo.txt #rm: elimina (remove) archivos
---
+++ script2.sh
@@ -0,0 +1,4 @@
+uname -a #Uname: Muestra informacion del sistema #-a: Muestra TODA la informacion del sistema
+lscpu #lscpu: Lista informacion detallada de la CPU
+free -h #free: Muestra informacion sobre la memoria RAM #-h (human-readable):Muestra los valores en formato legible
+df -h #df:Muestra informacion sobre el espacioen disco (disk filesystem) #-h (human-readable): Formato Legible 
